1cf820933b2470719fc9dc9837f047afe1e2464b
LJCK <50812564+LJCK@users.noreply.github.com>
2022-02-26 18:27:58 +0800

    Update Post.js

--- src/ComponentPage/Post.js
+++ src/ComponentPage/Post.js
@@ -1,47 +1,66 @@
-import { Link } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { TextField } from "@material-ui/core";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function Post() {
-  const { id, index } = useParams()
-  const [module, setModule] = useState([])
-  const [post, setPost] = useState([])
-  const [replyArea, setReplyArea] = useState(false)
+  const { id, index } = useParams();
+  const [module, setModule] = useState([]);
+  const [post, setPost] = useState([]);
+  const [reply, setReply] = useState([]);
+  const [comment, setComment] = useState([]);
+
+  const [replyArea, setReplyArea] = useState(false);
+
+  const handleReply = (e) => {
+    e.preventDefault();
+    console.log(comment);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setReply(e.target.value);
+  };
+
+  // const changeComment = (comments) => {
+  //   setComment(comments);
+  // };
 
   useEffect(() => {
-    const source = axios.CancelToken.source()
+    const source = axios.CancelToken.source();
     axios.get(`http://localhost:3000/module/${id}`).then((res) => {
-      setModule(res.data)
-      setPost(res.data.post[index])
-      console.log(post)
-    })
+      setModule(res.data);
+      setPost(res.data.post[index]);
+      //changeComment(res.data.post[index].comment);
+      setComment(res.data.post[index].comment);
+    });
 
     return () => {
-      source.cancel()
-    }
-  }, [])
+      source.cancel();
+    };
+  }, []);
 
   return (
     <div>
-      <div className='navigate'>
+      <div className="navigate">
         <span>
-          <Link to={'/forum'}>SCast Forum - Forum</Link> {'>>'}{' '}
-          <Link to={`/forum/${id}`}>{module.moduleCode}</Link> {'>>'}{' '}
+          <Link to={"/forum"}>SCast Forum - Forum</Link> {">>"}{" "}
+          <Link to={`/forum/${id}`}>{module.moduleCode}</Link> {">>"}{" "}
           {post.postTitle}
         </span>
       </div>
-
-      <div className='topic-container'>
-        <div className='head'>
-          <div className='authors'>Author</div>
-          <div className='content'>
+      {/*========================author area=========================*/}
+      <div className="topic-container">
+        <div className="head">
+          <div className="authors">Author</div>
+          <div className="content">
             {post.postType} : {post.postTitle}
           </div>
-          <div className='postreply'>
+          <div className="postreply">
             <button
               onClick={() => {
-                setReplyArea(!replyArea)
+                setReplyArea(!replyArea);
               }}
             >
               Post Reply
@@ -49,13 +68,13 @@ export default function Post() {
           </div>
         </div>
 
-        <div className='body'>
-          <div className='authors'>
+        <div className="body">
+          <div className="authors">
             <img
-              src='https://cdn.pixabay.com/photo/2014/04/05/13/05/boy-317041__340.jpg'
-              alt=''
+              src="https://cdn.pixabay.com/photo/2014/04/05/13/05/boy-317041__340.jpg"
+              alt=""
             ></img>
-            <div className='username'>
+            <div className="username">
               Posted By
               <br />
               {post.userId}
@@ -64,31 +83,66 @@ export default function Post() {
             </div>
           </div>
 
-          <div className='content'>
+          <div className="content">
             {post.postObjective}
             <br />
-            {post.postImage ? 'Image' : ''}
+            {post.postImage ? "Image" : ""}
             <br />
-            {post.postImage ? <img src={`${post.postImage}`} alt=''></img> : ''}
+            {post.postImage ? <img src={`${post.postImage}`} alt=""></img> : ""}
             {/* <hr />
             Regards {post.userId} */}
           </div>
         </div>
+
+        {/*========================comment area=========================*/}
+        <section>
+          {comment.map((data, index) => (
+            <div key={index}>
+              <div className="head">Comment</div>
+              <div className="body">
+                <div className="authors">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2014/04/05/13/05/boy-317041__340.jpg"
+                    alt=""
+                  ></img>
+                  <div className="username">
+                    Posted By
+                    <br />
+                    {data.userId}
+                  </div>
+                </div>
+
+                <div className="content">
+                  {data.commentText}
+                  <br />
+                  {data.commentImage ? "Image" : ""}
+                  <br />
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
       </div>
 
       {/* Reply area */}
       {replyArea ? (
-        <div className='comment-area'>
-          <textarea
-            name='reply'
-            id=''
-            placeholder='reply to post... '
-          ></textarea>
-          <input type='submit' name='' id='' value='submit'></input>
+        <div>
+          <form>
+            <TextField
+              style={{
+                background: "white",
+              }}
+              name="reply"
+              id=""
+              placeholder="reply to post... "
+              onChange={handleChange}
+            />
+            <Button onClick={handleReply}>click me</Button>
+          </form>
         </div>
       ) : (
-        ''
+        ""
       )}
     </div>
-  )
+  );
 }
