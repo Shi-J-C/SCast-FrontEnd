d1cfcea2846899b8789c4e2cb96ab3126fe4ac56
Shi_J_C <76093941+Shi-J-C@users.noreply.github.com>
2022-03-15 12:32:57 +0800

    add/delete bookmark function

--- src/ComponentPage/ForumDetail.js
+++ src/ComponentPage/ForumDetail.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import { MdOutlineForum } from 'react-icons/md'
+import React, { useEffect, useState } from 'react'
+import {
+  MdOutlineForum,
+  MdOutlineBookmarkAdd,
+  MdOutlineBookmarkRemove,
+} from 'react-icons/md'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function ForumDetail({
   _id,
@@ -11,6 +16,58 @@ export default function ForumDetail({
 }) {
   var str = createdAt
   var res = str.substring(0, 10)
+
+  const [userBookmark, setUserBookmark] = useState([])
+
+  let user = JSON.parse(sessionStorage.getItem('user'))
+  var newdata = {}
+  useEffect(() => {
+    async function fetchUserData() {
+      let response = await axios.get(`http://localhost:3000/user/${user._id}`)
+      setUserBookmark(response.data.bookmark)
+    }
+    fetchUserData()
+  }, [newdata])
+
+  const handleAddBookmark = () => {
+    newdata = { bookmarkId: _id }
+    console.log('data pass over:', newdata)
+    console.log(
+      'API Link:',
+      `http://localhost:3000/user/${user._id}/addBookmark`
+    )
+    axios.post(`http://localhost:3000/user/${user._id}/addBookmark`, newdata)
+  }
+
+  const handleDeleteBookmark = () => {
+    newdata = { bookmarkId: _id }
+    console.log('data pass over:', newdata)
+    console.log(
+      'API Link:',
+      `http://localhost:3000/user/${user._id}/deleteBookmark`
+    )
+    axios.post(`http://localhost:3000/user/${user._id}/deleteBookmark`, newdata)
+  }
+
+  const bookmarkCheck = () => {
+    let bookmarkExist = userBookmark.indexOf(`${_id}`)
+    if (bookmarkExist >= 0)
+      return (
+        <MdOutlineBookmarkRemove
+          onClick={() => {
+            handleDeleteBookmark()
+          }}
+        />
+      )
+    else
+      return (
+        <MdOutlineBookmarkAdd
+          onClick={() => {
+            handleAddBookmark()
+          }}
+        />
+      )
+  }
 
   return (
     <div className='subforum-row'>
@@ -23,12 +80,15 @@ export default function ForumDetail({
         </h1>
         <p>{moduleName}</p>
       </div>
+      <div className='subforum-icon subforum-column center'>
+        {/* <Bookmark _id={_id} userBookmark={userBookmark} /> */}
+        {bookmarkCheck()}
+      </div>
       <div className='subforum-stats subforum-column center'>
         <span>
           {'Module created at'}
           <br />
           {res}
-          {/* 25 Posts | 15 Replies */}
         </span>
       </div>
       <div className='subforum-info subforum-column center'>
