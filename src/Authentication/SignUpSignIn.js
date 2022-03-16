a5403bd5b62d080032fadcfebe28854a770b387b
Shi_J_C <76093941+Shi-J-C@users.noreply.github.com>
2022-03-16 18:01:47 +0800

    user update and visibility of password during signup

--- src/Authentication/SignUpSignIn.js
+++ src/Authentication/SignUpSignIn.js
@@ -1,38 +1,43 @@
-import React, { useState } from "react";
-import LoginIcon from "@mui/icons-material/Login";
-import LockIcon from "@mui/icons-material/Lock";
-import { TextField } from "@material-ui/core";
-import { Paper, Container, Avatar, Typography, Button } from "@mui/material";
-import { Grid } from "@material-ui/core";
-import FileBase from "react-file-base64";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import LoginIcon from '@mui/icons-material/Login'
+import LockIcon from '@mui/icons-material/Lock'
+import { TextField } from '@material-ui/core'
+import { Paper, Container, Avatar, Typography, Button } from '@mui/material'
+import { Grid } from '@material-ui/core'
+import FileBase from 'react-file-base64'
+import { useNavigate } from 'react-router-dom'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import VisibilityIcon from '@mui/icons-material/Visibility'
 
-import axios from "axios";
+import axios from 'axios'
 
 const SignUpSignIn = () => {
-  let navigate = useNavigate();
+  const [passwordShown, setPasswordShown] = useState(false)
+  let navigate = useNavigate()
   const initialState = {
-    name: "",
-    username: "",
-    userimage: "",
-    phoneNo: "",
-    telegramId: "",
-    password: "",
-    confirmPassword: "",
-  };
-  const [isSignup, setIsSignup] = useState(true);
-  const [form, setForm] = useState(initialState);
+    name: '',
+    username: '',
+    userimage: '',
+    phoneNo: '',
+    telegramId: '',
+    password: '',
+    confirmPassword: '',
+  }
+  const [isSignup, setIsSignup] = useState(true)
+  const [form, setForm] = useState(initialState)
 
   const switchMode = () => {
-    setForm(initialState);
-    setIsSignup((prevIsSignup) => !prevIsSignup);
-  };
+    setForm(initialState)
+    setIsSignup((prevIsSignup) => !prevIsSignup)
+  }
 
   const handleChange = (e) =>
-    setForm({ ...form, [e.target.name]: e.target.value });
+    setForm({ ...form, [e.target.name]: e.target.value })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (isSignup) {
       if (form.password === form.confirmPassword) {
@@ -43,120 +48,138 @@ const SignUpSignIn = () => {
           password: form.password,
           phoneNo: form.phoneNo,
           telegramId: form.telegramId,
-        };
+        }
         axios
           .post(`http://localhost:3000/user/addUser`, temp)
-          .then((res) => setForm(initialState));
+          .then((res) => setForm(initialState))
       } else {
-        alert("Your repeat password is different!!");
+        alert('Your repeat password is different!!')
       }
     } else {
       let temp = {
         username: form.username,
         password: form.password,
-      };
+      }
       axios
         .post(`http://localhost:3000/user/login`, temp)
         .then((res) => {
-          sessionStorage.setItem("accessToken", res.data.token);
-          sessionStorage.setItem("user", JSON.stringify(res.data.user));
-          navigate("/", { replace: true });
+          sessionStorage.setItem('accessToken', res.data.token)
+          sessionStorage.setItem('user', JSON.stringify(res.data.user))
+          navigate('/', { replace: true })
         })
         .catch(function (error) {
-          alert(error.response.data.message);
-        });
+          alert(error.response.data.message)
+        })
     }
-  };
+  }
 
   return (
     <>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <Paper
           elevation={3}
           style={{
-            border: "solid 1px #52057b",
-            marginTop: "20px",
-            padding: "20px",
-            justifyContent: "center",
-            boxShadow: "1px 2xp 3px #52057b",
+            border: 'solid 1px #52057b',
+            marginTop: '20px',
+            padding: '20px',
+            justifyContent: 'center',
+            boxShadow: '1px 2xp 3px #52057b',
           }}
         >
           <Avatar>{isSignup ? <LockIcon /> : <LoginIcon />}</Avatar>
           <Typography
-            component="h1"
-            variant="h5"
-            style={{ marginBottom: "10px" }}
+            component='h1'
+            variant='h5'
+            style={{ marginBottom: '10px' }}
           >
-            {isSignup ? "User Sign up" : "User Sign in"}
+            {isSignup ? 'User Sign up' : 'User Sign in'}
           </Typography>
           <form onSubmit={handleSubmit}>
             <Grid
               container
               spacing={2}
-              style={{ display: "flex", flexDirection: "column" }}
+              style={{ display: 'flex', flexDirection: 'column' }}
             >
               {isSignup && ( // only if signedup, show details.
                 <>
                   <TextField
-                    name="name"
-                    label="Name"
+                    name='name'
+                    label='Name'
                     value={form.name}
                     onChange={handleChange}
-                    type="text"
+                    type='text'
                     required
                     autoFocus
                   />
                 </>
               )}
               <TextField
-                name="username"
-                label="Email Address"
+                name='username'
+                label='Email Address'
                 value={form.username}
                 onChange={handleChange}
                 required
-                type="email"
+                type='email'
               />
               {isSignup && (
                 <>
                   <TextField
-                    name="phoneNo"
-                    label="Phone Number"
+                    name='phoneNo'
+                    label='Phone Number'
                     value={form.phoneNo}
                     onChange={handleChange}
                     required
-                    type="number"
+                    type='number'
                   />
                   <TextField
-                    name="telegramId"
-                    label="Telegram ID"
+                    name='telegramId'
+                    label='Telegram ID'
                     value={form.telegramId}
                     onChange={handleChange}
                     required
-                    type="text"
+                    type='text'
                   />
                 </>
               )}
 
               <TextField
-                name="password"
-                label="Password"
+                name='password'
+                label='Password'
                 value={form.password}
                 onChange={handleChange}
                 required
-                type="password"
+                type={passwordShown ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton
+                        onClick={() => {
+                          setPasswordShown(!passwordShown)
+                          console.log(passwordShown)
+                        }}
+                      >
+                        {passwordShown ? (
+                          <VisibilityIcon />
+                        ) : (
+                          <VisibilityOffIcon />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
               {isSignup && (
                 <>
                   <TextField
-                    name="confirmPassword"
-                    label="Repeat Password"
+                    name='confirmPassword'
+                    label='Repeat Password'
                     value={form.confirmPassword}
                     required
                     onChange={handleChange}
-                    type="password"
+                    type='password'
                   />
                   <FileBase
-                    type="file"
+                    type='file'
                     multiple={false}
                     onDone={({ base64 }) =>
                       setForm({ ...form, userimage: base64 })
@@ -166,20 +189,20 @@ const SignUpSignIn = () => {
               )}
             </Grid>
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="primary"
-              style={{ marginTop: "20px" }}
+              variant='contained'
+              color='primary'
+              style={{ marginTop: '20px' }}
             >
-              {isSignup ? "Sign Up" : "Sign In"}
+              {isSignup ? 'Sign Up' : 'Sign In'}
             </Button>
 
-            <Grid container alignItems="flex-end">
+            <Grid container alignItems='flex-end'>
               <Grid item>
                 <Button onClick={switchMode}>
                   {isSignup
-                    ? "Already have an account? Sign in"
+                    ? 'Already have an account? Sign in'
                     : "Don't have an account? Sign Up"}
                 </Button>
               </Grid>
@@ -188,7 +211,7 @@ const SignUpSignIn = () => {
         </Paper>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default SignUpSignIn;
+export default SignUpSignIn
--- src/ComponentPage/Profile.js
+++ src/ComponentPage/Profile.js
@@ -1,38 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { TextField, Grid } from "@material-ui/core";
-import { Paper, Container, Button } from "@mui/material";
-import { useParams } from "react-router-dom";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import axios from "axios";
-import Checkbox from "@mui/material/Checkbox";
+import React, { useState, useEffect } from 'react'
+import { TextField, Grid } from '@material-ui/core'
+import { Paper, Container, Button } from '@mui/material'
+import { useParams } from 'react-router-dom'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import axios from 'axios'
+import Checkbox from '@mui/material/Checkbox'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import VisibilityIcon from '@mui/icons-material/Visibility'
 
 const Profile = () => {
+  var user = JSON.parse(sessionStorage.getItem('user'))
+  const [passwordShown, setPasswordShown] = useState(false)
+
   const initialState = {
-    name: "",
-    username: "",
-    phoneNo: "",
-    telegramId: "",
-    password: "",
-    confirmPassword: "",
-  };
-  const [userProfile, setUserProfile] = useState(initialState);
+    name: '',
+    username: '',
+    phoneNo: '',
+    telegramId: '',
+    password: user.password,
+    confirmPassword: user.password,
+  }
+  const [userProfile, setUserProfile] = useState(initialState)
 
-  const [changePassword, setChangePassword] = useState(false);
-  const { id } = useParams();
+  const [changePassword, setChangePassword] = useState(false)
+  const { id } = useParams()
 
-  const handleChange = (e) => {
-    setUserProfile({ ...userProfile, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   setUserProfile({ ...userProfile, [e.target.name]: e.target.value })
+  // }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    setPasswordShown(false)
     // call update backend
-    console.log(userProfile);
-  };
+    axios.post(`http://localhost:3000/user/${id}/updateUser`, userProfile)
+    console.log(userProfile)
+  }
 
   useEffect(() => {
     async function fetchUserData() {
-      let response = await axios.get(`http://localhost:3000/user/${id}`);
+      let response = await axios.get(`http://localhost:3000/user/${id}`)
 
       setUserProfile({
         ...userProfile,
@@ -40,93 +49,149 @@ const Profile = () => {
         username: response.data.username,
         phoneNo: response.data.phoneNo,
         telegramId: response.data.telegramId,
-      });
+      })
     }
-    fetchUserData();
-  }, []);
+    fetchUserData()
+  }, [])
 
   return (
     <div>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <Paper
           elevation={3}
           style={{
-            border: "solid 1px #52057b",
-            marginTop: "20px",
-            padding: "20px",
-            justifyContent: "center",
-            boxShadow: "1px 2xp 3px #52057b",
+            border: 'solid 1px #52057b',
+            marginTop: '20px',
+            padding: '20px',
+            justifyContent: 'center',
+            boxShadow: '1px 2xp 3px #52057b',
           }}
         >
           <form onSubmit={handleSubmit}>
             <Grid
               container
               spacing={2}
-              style={{ display: "flex", flexDirection: "column" }}
+              style={{ display: 'flex', flexDirection: 'column' }}
             >
               <TextField
-                name="name"
-                label="Name"
+                name='name'
+                label='Name'
                 value={userProfile.name}
-                onChange={handleChange}
+                // onChange={handleChange}
+                onChange={(e) =>
+                  setUserProfile({
+                    ...userProfile,
+                    name: e.target.value,
+                  })
+                }
                 autoFocus
               />
 
               <TextField
-                name="username"
-                label="Email Address"
+                name='username'
+                label='Email Address'
                 value={userProfile.username}
-                onChange={handleChange}
-                type="email"
+                onChange={(e) =>
+                  setUserProfile({
+                    ...userProfile,
+                    username: e.target.value,
+                  })
+                }
+                // onChange={handleChange}
+
+                type='email'
               />
 
               <TextField
-                name="phoneNo"
-                label="Phone Number"
+                name='phoneNo'
+                label='Phone Number'
                 value={userProfile.phoneNo}
-                onChange={handleChange}
+                // onChange={handleChange}
+                onChange={(e) =>
+                  setUserProfile({
+                    ...userProfile,
+                    phoneNo: e.target.value,
+                  })
+                }
                 required
-                type="number"
+                type='number'
               />
               <TextField
-                name="telegramId"
-                label="Telegram ID"
+                name='telegramId'
+                label='Telegram ID'
                 value={userProfile.telegramId}
-                onChange={handleChange}
+                // onChange={handleChange}
+                onChange={(e) =>
+                  setUserProfile({
+                    ...userProfile,
+                    telegramId: e.target.value,
+                  })
+                }
                 required
-                type="text"
+                type='text'
               />
 
               {changePassword && (
                 <>
                   <TextField
-                    name="password"
-                    label="Password"
+                    name='password'
+                    label='Password'
                     value={userProfile.password}
-                    onChange={handleChange}
-                    type="password"
+                    // onChange={handleChange}
+                    onChange={(e) =>
+                      setUserProfile({
+                        ...userProfile,
+                        password: e.target.value,
+                      })
+                    }
+                    type={passwordShown ? 'text' : 'password'}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position='end'>
+                          <IconButton
+                            onClick={() => {
+                              setPasswordShown(!passwordShown)
+                              console.log(passwordShown)
+                            }}
+                          >
+                            {passwordShown ? (
+                              <VisibilityIcon />
+                            ) : (
+                              <VisibilityOffIcon />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
                   />
+
                   <TextField
-                    name="confirmPassword"
-                    label="Repeat Password"
+                    name='confirmPassword'
+                    label='Repeat Password'
                     value={userProfile.confirmPassword}
-                    onChange={handleChange}
-                    type="password"
+                    // onChange={handleChange}
+                    onChange={(e) =>
+                      setUserProfile({
+                        ...userProfile,
+                        confirmPassword: e.target.value,
+                      })
+                    }
+                    type={passwordShown ? 'text' : 'password'}
                   />
                 </>
               )}
               <FormControlLabel
                 control={<Checkbox />}
                 onChange={() => setChangePassword(!changePassword)}
-                label="Change password"
+                label='Change password'
               />
             </Grid>
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
-              color="primary"
-              style={{ marginTop: "20px" }}
+              variant='contained'
+              color='primary'
+              style={{ marginTop: '20px' }}
               onClick={() => handleSubmit}
             >
               Submit
@@ -135,7 +200,7 @@ const Profile = () => {
         </Paper>
       </Container>
     </div>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
