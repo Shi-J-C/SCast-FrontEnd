578a9b5ee0699b6cf068e8f8bedef4a918d1c663
Shi_J_C <76093941+Shi-J-C@users.noreply.github.com>
2022-03-19 16:14:24 +0800

    moving img to img folder

--- src/Nav.js
+++ src/Nav.js
@@ -1,123 +1,123 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { green } from "@mui/material/colors";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import { useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import Logo from "./Logo.png";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { green } from '@mui/material/colors'
+import Menu from '@mui/material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import Logo from './image/Logo.png'
 
 const Nav = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null)
 
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
   const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElUser(event.currentTarget)
+  }
 
   const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    setAnchorElUser(null)
+  }
 
   function stringAvatar(name) {
     return {
       sx: {
         bgcolor: green[500],
       },
-      children: `${name.split(" ")[0][0]}`,
-    };
+      children: `${name.split(' ')[0][0]}`,
+    }
   }
 
-  const [userProfile, setUserProfile] = useState("");
-  const [showAdminDashBoard, setShowAdminDashBoard] = useState(false);
-  const location = useLocation();
+  const [userProfile, setUserProfile] = useState('')
+  const [showAdminDashBoard, setShowAdminDashBoard] = useState(false)
+  const location = useLocation()
 
   const logout = () => {
-    sessionStorage.clear();
-    setUserProfile(null);
-    navigate("/", { replace: true });
-  };
+    sessionStorage.clear()
+    setUserProfile(null)
+    navigate('/', { replace: true })
+  }
 
   useEffect(() => {
-    let user = JSON.parse(sessionStorage.getItem("user"));
-    if (user?.role === "student") {
+    let user = JSON.parse(sessionStorage.getItem('user'))
+    if (user?.role === 'student') {
       async function fetchUserData() {
         let response = await axios.get(
           `http://localhost:3000/user/${user?._id}`
-        );
+        )
 
-        setUserProfile(response.data);
+        setUserProfile(response.data)
       }
-      fetchUserData();
+      fetchUserData()
     }
 
-    if (user?.role === "administrator") {
+    if (user?.role === 'administrator') {
       async function fetchUserData() {
         let response = await axios.get(
           `http://localhost:3000/user/${user?._id}`
-        );
+        )
 
-        setUserProfile(response.data);
+        setUserProfile(response.data)
       }
-      fetchUserData();
-      setShowAdminDashBoard(true);
+      fetchUserData()
+      setShowAdminDashBoard(true)
     } else {
-      setShowAdminDashBoard(false);
+      setShowAdminDashBoard(false)
     }
-  }, [location]);
+  }, [location])
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
         <Toolbar disableGutters>
           <Typography
-            variant="h6"
+            variant='h6'
             noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            component='div'
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
             <Avatar>
               <img
-                style={{ width: "80px", hight: "80px" }}
+                style={{ width: '80px', hight: '80px' }}
                 src={Logo}
-                alt="Logo"
+                alt='Logo'
               />
             </Avatar>
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             <Button
-              onClick={() => navigate("/", { replace: true })}
-              sx={{ my: 2, color: "white", display: "block" }}
+              onClick={() => navigate('/', { replace: true })}
+              sx={{ my: 2, color: 'white', display: 'block' }}
             >
               Home
             </Button>
             <Button
-              onClick={() => navigate("/about", { replace: true })}
-              sx={{ my: 2, color: "white", display: "block" }}
+              onClick={() => navigate('/about', { replace: true })}
+              sx={{ my: 2, color: 'white', display: 'block' }}
             >
               About
             </Button>
             <Button
-              onClick={() => navigate("/forum", { replace: true })}
-              sx={{ my: 2, color: "white", display: "block" }}
+              onClick={() => navigate('/forum', { replace: true })}
+              sx={{ my: 2, color: 'white', display: 'block' }}
             >
               Forum
             </Button>
             {showAdminDashBoard && (
               <Button
-                onClick={() => navigate("/updatemodule")}
-                sx={{ my: 2, color: "white", display: "block" }}
+                onClick={() => navigate('/updatemodule')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Admin DashBoard
               </Button>
@@ -125,14 +125,14 @@ const Nav = () => {
             {userProfile ? (
               <Button
                 onClick={logout}
-                sx={{ my: 2, color: "white", display: "block" }}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Log Out
               </Button>
             ) : (
               <Button
-                onClick={() => navigate("/auth", { replace: true })}
-                sx={{ my: 2, color: "white", display: "block" }}
+                onClick={() => navigate('/auth', { replace: true })}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 Sign Up
               </Button>
@@ -140,26 +140,26 @@ const Nav = () => {
           </Box>
           {userProfile && (
             <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
+              <Tooltip title='Open settings'>
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <Avatar
-                    alt="Remy Sharp"
+                    alt='Remy Sharp'
                     {...stringAvatar(userProfile.name)}
                   />
                 </IconButton>
               </Tooltip>
               <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
+                sx={{ mt: '45px' }}
+                id='menu-appbar'
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
@@ -169,15 +169,15 @@ const Nav = () => {
                     navigate(`/profile/${userProfile._id}`, { replace: true })
                   }
                 >
-                  <Typography textAlign="center">Profile</Typography>
+                  <Typography textAlign='center'>Profile</Typography>
                 </MenuItem>
                 <MenuItem
-                  onClick={() => navigate("/bookmark", { replace: true })}
+                  onClick={() => navigate('/bookmark', { replace: true })}
                 >
-                  <Typography textAlign="center">Bookmark</Typography>
+                  <Typography textAlign='center'>Bookmark</Typography>
                 </MenuItem>
                 <MenuItem onClick={logout}>
-                  <Typography textAlign="center">Log out</Typography>
+                  <Typography textAlign='center'>Log out</Typography>
                 </MenuItem>
               </Menu>
             </Box>
@@ -185,6 +185,6 @@ const Nav = () => {
         </Toolbar>
       </Container>
     </AppBar>
-  );
-};
-export default Nav;
+  )
+}
+export default Nav
